0e36b5c30f99ae0486b65ed7f084de237e4eefb2
Bonnie <moller.bon@gmail.com>
2024-04-15 13:53:23 +0200

    UseToggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Counter from "./Components/Counter"
+import ToggleBtn from "./Components/ToggleBtn"
 
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <Counter initialCount={5} minValue={-7} maxValue={58} stepUp={20} stepDown={11}/>
     <Counter initialCount={0} minValue={-10} maxValue={20} stepUp={3} stepDown={2}/>
     <Counter initialCount={30} minValue={0} maxValue={100} stepUp={40} stepDown={30}/>
+    <ToggleBtn/>
     </>
   )
 }
