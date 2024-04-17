c7cdaea2f4bd181166232fd3881fbc00ab49bd66
Bonnie <moller.bon@gmail.com>
2024-04-17 10:58:59 +0200

    back to ternary with error

--- src/Hooks/UseCounter.ts
+++ src/Hooks/UseCounter.ts
@@ -3,7 +3,6 @@ import { useState } from "react";
 export type CounterTypes = {
   [key: string]: number;
 };
-
 export const UseCounter = ({
   minValue,
   maxValue,
@@ -11,21 +10,16 @@ export const UseCounter = ({
 }: CounterTypes) => {
   const [count, setCount] = useState<number>(initialCount);
   const increment = (num: number) => {
-    if (count + num > maxValue) {
-      alert(`Sorry, max value is ${maxValue}`);
-    } else {
-      setCount((c) => c + num);
-    }
+    count + num > maxValue
+      ? alert(`Sorry, max value is ${maxValue}`)
+      : setCount((c) => c + num);
   };
   const decrement = (num: number) => {
-    if (count - num < minValue) {
-      alert(`Sorry, min value is ${minValue}`);
-    } else {
-      setCount((c) => c - num);
-    }
+    count - num < minValue
+      ? alert(`Sorry, min value is ${minValue}`)
+      : setCount((c) => c - num);
   };
   const reset = () => setCount(initialCount);
   const isEven = () => (count % 2 === 0 ? true : false);
-
   return { count, increment, decrement, reset, isEven };
 };
